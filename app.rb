d26f44bd2bc790a6a77874a2ee6a504c5d830d04
LandonWest <landonwest5@gmail.com>
2016-02-29 16:17:11 -0800

    add Pony gem and configure for contact mailer

--- app.rb
+++ app.rb
@@ -19,3 +19,29 @@ end
 get '/contact' do
   erb :contact
 end
+
+post '/contact' do
+  require 'pony'
+  Pony.mail({
+    :to => 'landonwest5@gmail.com',
+    :from => params[:email],
+    :subject => params[:name] + ' has left you a message!',
+    :body => 'Name: ' + params[:name] + '; ' + 'Email: ' + params[:email] + '; ' + 'Message: ' + params[:message],
+    :via => :smtp,
+    :via_options => {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :enable_starttls_auto => true,
+      :user_name            => ENV['GMAIL_USERNAME'],
+      :password             => ENV['GMAIL_PASSWORD'],
+      :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+      :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    }
+  })
+  redirect '/success'
+end
+
+get '/success' do
+  @notification = "Thanks, Your message has been sent!"
+  erb :index
+end
